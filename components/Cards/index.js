663a0ead9b64240c7353f37804a8851faf51852e
Mcguffinn <deronviledwin@gmail.com>
2020-01-17 12:46:29 -0500

    axios is broken no way of knowing if the changes im making are working so thats nice

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,41 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then( response =>{
+    console.log(response.data);
+    Object.values(response.data.articles).forEach(item =>{
+        console.log(item);
+        item.forEach(items =>{
+            createArt(items.headline, items.authorName, items.authorPhoto)
+        })
+    })
+})
+
+
+function createArt(authLine, authName, authPhoto){
+
+    const cards = document.createElement('div');
+    const headline = document.createElement('div');
+    const auth = document.createElement('div');
+    const imgDiv = document.createElement('div');
+    const img = document.createElement('img');
+    const spanner = document.createElement('span');
+
+    cards.classList.add('card');
+    headline.classList.add('headline');
+    auth.classList.add('author');
+    imgDiv.classList.add('img-container');
+
+    cards.appendChild(headline);
+    cards.appendChild(auth);
+    auth.appendChild(imgDiv);
+    imgDiv.append(img);
+    auth.appendChild(spanner);
+
+    headline.innerText = authLine;
+    spanner.textContent = 'By: ' + authName;
+    img.src = authPhoto;
+
+    document.querySelector('.cards-container').appendChild(cards);
+}
